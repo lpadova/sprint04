03f14e7b90e90c20de54fb9a9536b44877f44c8a
Luis Felipe Robbo <61969628+Luisrobbo22@users.noreply.github.com>
2021-09-07 20:16:04 -0300

    commit inicial

---
+++ src/main/java/br/com/fiap/challenge03/Challenge03Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challenge03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Challenge03Application.class, args);
+    }
+
+}
