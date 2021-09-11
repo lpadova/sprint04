67814ebb76a8235d67b4d5598583389bc96c3b61
Luis Felipe Robbo <61969628+Luisrobbo22@users.noreply.github.com>
2021-09-11 13:47:02 -0300

    Add files via upload

---
+++ Estado.java
@@ -0,0 +1,59 @@
+package br.com.fiap.challenge03.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.util.Calendar;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Estado implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_estado")
+    private Integer id;
+
+    @Column(name = "nr_atualizacao", nullable = false)
+    @NotBlank(message = "Campo nr_atualização obrigatório")
+    private Integer numeroAtualizacao;
+
+    @ManyToOne()
+    @JoinColumn(name = "id_usuario", referencedColumnName = "id_usuario")
+    @JsonBackReference
+    private Usuario usuario;
+
+    @CreationTimestamp
+    @Column(name = "dt_atualizacao", nullable = false)
+    @NotBlank(message = "Campo dt_atualizacao é obrigatório")
+    private Calendar dataAtualizacao;
+
+    @Column(name = "md_pressao_arterial", length = 10)
+    @NotBlank(message = "O campo md_pressao_arterial é obrigatório")
+    private String pressaoArterial;
+
+    @Column(name = "md_monitoramento_cardiaco", length = 10)
+    @NotBlank(message = "O campo md_monitoramento_cardiaco é obrigatório")
+    private String monitoramentoCardiaco;
+
+    @Column(name = "md_temperatura", length = 10)
+    private String temperatura;
+
+    public Estado(Integer id, Calendar dataAtualizacao, String temperatura,
+                  String monitoramentoCardiaco, String pressaoArterial) {
+        this.id = id;
+        this.dataAtualizacao = dataAtualizacao;
+        this.temperatura = temperatura;
+        this.monitoramentoCardiaco = monitoramentoCardiaco;
+        this.pressaoArterial = pressaoArterial;
+    }
+}
